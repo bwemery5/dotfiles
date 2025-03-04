09c99c7f1892399028985eb133ad7379219de870
Ben Emery <bemery@zoll.com>
2025-03-04 15:42:01 -0500

    disable inlay

--- nvim/.config/nvim/lua/plugins/editcfg.lua
+++ nvim/.config/nvim/lua/plugins/editcfg.lua
@@ -22,6 +22,12 @@ return {
             },
         }
     },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            inlay_hints = { enabled = false },
+        },
+    },
     -- {
     --     "catppuccin/nvim",
     --     opts = {
